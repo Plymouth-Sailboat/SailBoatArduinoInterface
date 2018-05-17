00056e9e0281e3c8138623486ff0cbdfcc8d5806
Ulysse Vautier <ulysse.vautier@gmail.com>
2018-05-17 16:42:16 +0100

    Radio controller ergonomy

--- AutonomousSailboat/RCControl.cpp
+++ AutonomousSailboat/RCControl.cpp
@@ -10,12 +10,12 @@ void RCControl::init() {
 }
 
 void RCControl::Control(const geometry_msgs::Twist& cmd) {
-  float sail = Sailboat::Instance()->getRC()->getValue(RC_2);
+  float sail = Sailboat::Instance()->getRC()->getValue(RC_3);
   float rudder = Sailboat::Instance()->getRC()->getValue(RC_1);
-  float ch3 = Sailboat::Instance()->getRC()->getValue(RC_3);
+  float ch2 = Sailboat::Instance()->getRC()->getValue(RC_2);
 
-  Sailboat::Instance()->getRudder()->applyCommand((rudder-0.5)*2.0*RUDDER_MAX);
-  if(ch3 < 0.5){
+  Sailboat::Instance()->getRudder()->applyCommand(-(rudder-0.5)*2.0*RUDDER_MAX);
+  if(ch2 < 0.5){
     Sailboat::Instance()->getSail()->applyCommand((sail-0.5)*2.0*SAIL_MAX);
   }else{
     WindSensor* wind = Sailboat::Instance()->getWindSensor();
