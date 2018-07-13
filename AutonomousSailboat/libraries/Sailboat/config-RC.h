5322a661fb51bb0d097ccf9a23ce6e1879a97171
Ulysse Vautier <ulysse.vautier@plymouth.ac.uk>
2018-07-13 12:49:17 +0100

    Added HK-TR6 RC_receiver and standardised RC_Control

--- AutonomousSailboat/libraries/Sailboat/config-RC.h
+++ AutonomousSailboat/libraries/Sailboat/config-RC.h
@@ -22,7 +22,7 @@
 
 //RC Config
 #ifdef FLYSKY
-#define RC_NUM_CHANNELS 6
+#define RC_NUM_CHANNELS 4
 #define RC_1_MIN    1012
 #define RC_1_MAX    1924
 #define RC_2_MIN    1008
@@ -35,10 +35,14 @@
 #define RC_5_MAX    0
 #define RC_6_MIN    0
 #define RC_6_MAX    0
+
+#define RC_RUDDER	RC_1
+#define RC_SAIL		RC_3
+#define RC_AUTO		RC_2
 #endif
 
 #ifdef J5C01R
-#define RC_NUM_CHANNELS 5
+#define RC_NUM_CHANNELS 4
 #define RC_1_MIN    1062
 #define RC_1_MAX    2062
 #define RC_2_MIN    1132
@@ -51,6 +55,30 @@
 #define RC_5_MAX    0
 #define RC_6_MIN    0
 #define RC_6_MAX    0
+
+#define RC_RUDDER	RC_1
+#define RC_SAIL		RC_3
+#define RC_AUTO		RC_2
+#endif
+
+#ifdef HK_TR6
+#define RC_NUM_CHANNELS 4
+#define RC_1_MIN    1092
+#define RC_1_MAX    1940
+#define RC_2_MIN    1100
+#define RC_2_MAX    1904
+#define RC_3_MIN    1104
+#define RC_3_MAX    1760
+#define RC_4_MIN    1888
+#define RC_4_MAX    1020
+#define RC_5_MIN    0
+#define RC_5_MAX    0
+#define RC_6_MIN    0
+#define RC_6_MAX    0
+
+#define RC_RUDDER	RC_1
+#define RC_SAIL		RC_3
+#define RC_AUTO		RC_2
 #endif
 
 #endif
