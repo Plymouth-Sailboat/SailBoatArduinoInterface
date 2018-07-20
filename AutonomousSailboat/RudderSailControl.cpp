468ad2b03fc57f3e292a0b831ad5873ed133582c
Ulysse VAUTIER <ulysse.vautier@plymouth.ac.uk>
2018-07-20 12:15:54 +0100

    Sail sign bug

--- AutonomousSailboat/RudderSailControl.cpp
+++ AutonomousSailboat/RudderSailControl.cpp
@@ -14,7 +14,7 @@ void RudderSail::Control(const geometry_msgs::Twist& cmd) {
   double sail = cmd.angular.y * RAD_TO_DEG;
   double rudder2 = cmd.angular.z * RAD_TO_DEG;
   Sailboat::Instance()->getRudder()->applyCommand(rudder);
-  Sailboat::Instance()->getSail()->applyCommand(sail);
+  Sailboat::Instance()->getSail()->applyCommand(abs(sail));
 #ifdef ACTUATOR_RUDDER2
   Sailboat::Instance()->getRudder2()->applyCommand(rudder2);
 #endif
