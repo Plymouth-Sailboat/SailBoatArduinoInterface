c01353eb298444e9aa2a58d94651a9cfbb736a6c
Ulysse VAUTIER <ulysse.vautier@plymouth.ac.uk>
2019-07-02 14:35:34 +0100

    Changed optimal sail of rudder control by estimating cap

--- AutonomousSailboat/RudderControl.cpp
+++ AutonomousSailboat/RudderControl.cpp
@@ -13,7 +13,7 @@ void RudderControl::Control(const geometry_msgs::Twist& cmd) {
   double rudder = cmd.angular.x * RAD_TO_DEG;
   double rudder2 = cmd.angular.z * RAD_TO_DEG;
 
-  //IMU *imu = Sailboat::Instance()->getIMU();
+  IMU *imu = Sailboat::Instance()->getIMU();
   
   Sailboat::Instance()->getRudder()->applyCommand(rudder);
 #ifdef ACTUATOR_RUDDER2
@@ -22,6 +22,9 @@ void RudderControl::Control(const geometry_msgs::Twist& cmd) {
   double sail = 0;
   WindSensor* wind = Sailboat::Instance()->getWindSensor();
 
-  sail = SAIL_MAX * (cos(wind->getMeasure()) + 1) / 2;
+  double theta = imu->getHeading();
+  double cap = theta-asin(cmd.angular.x/(RUDDER_MAX*DEG_TO_RAD));
+
+  sail = SAIL_MAX * (cos(wind->getMeasure() - cap) + 1) / 2;
   Sailboat::Instance()->getSail()->applyCommand(abs(sail));
 }
